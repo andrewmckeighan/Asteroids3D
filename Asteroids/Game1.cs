8ec3a1df41d70c37a2373db828122e48ba04d193
amm@iastate.edu <amm@iastate.edu>
2018-02-25 22:24:01 -0600

    Added camera movement and ship movement

--- Asteroids/Game1.cs
+++ Asteroids/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace Asteroids
 {
@@ -13,7 +14,7 @@ namespace Asteroids
         GraphicsDevice device;
         SpriteBatch spriteBatch;
         private Matrix world = Matrix.CreateTranslation(new Vector3(10, 0, 0));//Where the vertices are in relationship to the whole world.
-        private Matrix view = Matrix.CreateLookAt(new Vector3(-10, 0, 10), new Vector3(0, 0, 0), Vector3.UnitY);//Puts coordinates into view space. Where vertices are in relation to the viewer.
+        private Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), Vector3.UnitY);//Puts coordinates into view space. Where vertices are in relation to the viewer.
         private Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.1f, 1000f);//Where on the screen vertices should appear. Camera control.
         
         Effect effect;
@@ -21,8 +22,10 @@ namespace Asteroids
         private Texture2D playerTexture;
         private Model skyboxModel;
         private Texture2D skyboxTexture;
-        private Vector3 playerPosition;
-        private Quaternion playerRotation;
+        private Vector3 playerPosition = new Vector3(0,0,0);
+        private Quaternion playerRotation = Quaternion.Identity;
+        float gameSpeed = 1.0f;
+
 
         public Game1()
         {
@@ -81,6 +84,12 @@ namespace Asteroids
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            float moveSpeed = gameTime.ElapsedGameTime.Milliseconds / 80.0f * gameSpeed;
+            
+            ProcessKeyboard(gameTime, ref playerPosition);
+            
+            MoveForward(ref playerPosition, playerRotation, moveSpeed);
+            UpdateCamera();
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -93,19 +102,20 @@ namespace Asteroids
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Crimson);
-            DrawSkybox(skyboxModel, Matrix.CreateTranslation(new Vector3(0, 0, 0)), view, projection);
+            DrawSkybox(skyboxModel, view, projection);
             // TODO: Add your drawing code here
-            DrawPlayerModel(playerModel, world, view, projection);
+            DrawPlayerModel(playerModel, view, projection);
             base.Draw(gameTime);
         }
 
-        private void DrawPlayerModel(Model model, Matrix world, Matrix view, Matrix projection)
+        private void DrawPlayerModel(Model model, Matrix view, Matrix projection)
         {
+            Matrix worldMatrix = Matrix.CreateScale(0.01f, 0.01f, 0.01f) * Matrix.CreateRotationY(MathHelper.Pi) * Matrix.CreateFromQuaternion(playerRotation) * Matrix.CreateTranslation(playerPosition);
             foreach (ModelMesh mesh in model.Meshes)
             {
                 foreach (BasicEffect effect in mesh.Effects)
                 {
-                    effect.World = world;
+                    effect.World = worldMatrix;
                     effect.View = view;
                     effect.Projection = projection;
                     effect.EnableDefaultLighting();
@@ -117,7 +127,7 @@ namespace Asteroids
             }
         }
 
-        private void DrawSkybox(Model model, Matrix world, Matrix view, Matrix projection)
+        private void DrawSkybox(Model model,  Matrix view, Matrix projection)
         {
             foreach (ModelMesh mesh in model.Meshes)
             {
@@ -133,8 +143,55 @@ namespace Asteroids
                 mesh.Draw();
             }
         }
-        
-        
+
+        //help from http://www.riemers.net/eng/Tutorials/XNA/Csharp/Series2/Flight_kinematics.php
+        private void ProcessKeyboard(GameTime gameTime, ref Vector3 playerPosition)
+        {
+            float leftRightRot = 0;
+            float upDownRot = 0;
+
+            float turningSpeed = (float)gameTime.ElapsedGameTime.TotalMilliseconds / 2000.0f;
+            turningSpeed *= 1.6f * gameSpeed;
+            KeyboardState keys = Keyboard.GetState();
+
+            if (keys.IsKeyDown(Keys.D))
+                leftRightRot += turningSpeed;
+            if (keys.IsKeyDown(Keys.A))
+                leftRightRot -= turningSpeed;
+         
+            if (keys.IsKeyDown(Keys.S))
+                upDownRot += turningSpeed;
+            if (keys.IsKeyDown(Keys.W))
+                upDownRot -= turningSpeed;
+
+            
+
+            Quaternion additionalRot = Quaternion.CreateFromAxisAngle(new Vector3(0, 0, -1), leftRightRot) * Quaternion.CreateFromAxisAngle(new Vector3(1, 0, 0), upDownRot);
+            playerRotation *= additionalRot;
+        }
+        private void MoveForward(ref Vector3 position, Quaternion rotationQuat, float speed)
+        {
+            Vector3 addVector = Vector3.Transform(new Vector3(0, 0, -1), rotationQuat);
+            KeyboardState keys = Keyboard.GetState();
+            if (keys.IsKeyDown(Keys.LeftShift))
+                position -= addVector * speed;
+            if (keys.IsKeyDown(Keys.LeftControl))
+                position += addVector * speed;
+
+
+        }
+        private void UpdateCamera()
+        {
+            Vector3 campos = new Vector3(0, 0.2f, -0.6f);
+            campos = Vector3.Transform(campos, Matrix.CreateFromQuaternion(playerRotation));
+            campos += playerPosition;
+
+            Vector3 camup = new Vector3(0, 1, 0);
+            camup = Vector3.Transform(camup, Matrix.CreateFromQuaternion(playerRotation));
+
+            view = Matrix.CreateLookAt(campos, playerPosition, camup);
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, device.Viewport.AspectRatio, 0.2f, 500.0f);
+        }
 
     }
 }
